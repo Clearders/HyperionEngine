0deac1c3fcb797e0a281abeb0f1aa06c7f5f320c
Felix <1319190086@qq.com>
2025-12-24 23:33:58 +0800

    Log system fix

--- Hyperion/src/Hyperion.h
+++ Hyperion/src/Hyperion.h
@@ -2,6 +2,8 @@
 
 #include "Hyperion\Application.h"
 #include <stdio.h>
+#include "Hyperion/Log.h"
+#include <spdlog/spdlog.h>
 //---------------Entry point---------------
 #include "Hyperion\EntryPoint.h"
 //-----------------------------------------
--- Hyperion/src/Hyperion/EntryPoint.h
+++ Hyperion/src/Hyperion/EntryPoint.h
@@ -1,10 +1,15 @@
 ﻿#pragma once
 
 #ifdef HYPERION_PLATFORM_WINDOWS
+#include "Log.h"
 extern Hyperion::Application* CreateApplication();
 
     int main(int argc, char** argv)
     {
+        Hyperion::Log::Init();
+        Hyperion::Log::GetCoreLogger()->warn("Initialized Log!");
+        Hyperion::Log::GetClientLogger()->info("Hello!");
+        
         printf("Hyperion Engine\n");
         auto app = CreateApplication();
         app->Run();
